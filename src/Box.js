4a3c58f7786c094afa0c5aa903afbf5e73a06650
Binyameen <bmghafoor@gmail.com>
2023-06-08 13:04:39 -0400

    Button deletes Box on click

--- src/Box.js
+++ src/Box.js
@@ -1,12 +1,32 @@
 import React from "react";
 
 const Box = ({ backgroundColor, height, width }) => {
-  const style = {
+  const boxStyle = {
     backgroundColor,
     height: `${height}px`,
     width: `${width}px`,
+    border: "1px solid black",
   };
-  return <div style={style}></div>;
+
+  const buttonStyle = {
+    display: "inline-block",
+    cursor: "pointer",
+  };
+
+  const handleDelete = (e) => {
+    const parentElement = e.currentTarget.parentNode;
+    parentElement.remove();
+  };
+
+  return (
+    <>
+      <div style={boxStyle}>
+        <button style={buttonStyle} onClick={handleDelete}>
+          X
+        </button>
+      </div>
+    </>
+  );
 };
 
 export default Box;
