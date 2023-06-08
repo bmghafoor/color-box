c5c3c739055f412e1a26fbda2a23197f9644c13f
Binyameen <bmghafoor@gmail.com>
2023-06-08 13:33:24 -0400

    Tests for Components

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,25 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, fireEvent } from "@testing-library/react";
+import Box from "./Box";
+import BoxList from "./BoxList";
+import BoxForm from "./BoxForm";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it("renders without crashing", function () {
+  render(<Box />);
+  render(<BoxList />);
+  render(<BoxForm />);
+});
+
+it("matches snapshot", function () {
+  const { asFragment } = render(<Box />);
+  expect(asFragment()).toMatchSnapshot();
+});
+
+it("matches snapshot", function () {
+  const { asFragment } = render(<BoxList />);
+  expect(asFragment()).toMatchSnapshot();
+});
+
+it("matches snapshot", function () {
+  const { asFragment } = render(<BoxForm />);
+  expect(asFragment()).toMatchSnapshot();
 });
